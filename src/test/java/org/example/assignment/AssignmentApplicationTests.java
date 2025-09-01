b8729d10d41d2e6da8e74090112f6fd5d3443165
yeonjookang <96986782+yeonjookang@users.noreply.github.com>
2025-09-01 20:28:50 +0900

    feat: 프로젝트 초기화

---
+++ src/test/java/org/example/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
